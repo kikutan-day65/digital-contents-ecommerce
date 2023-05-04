eb17a2ced311892aa880133cb6ee3569fab2bfd4
Haruto Mori <baystarsman@outlook.jp>
2023-05-05 02:32:26 +0900

    feat: connect django to mySQL

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysql',
+        'USER': "root",
+        'PASSWORD': config('MySQL_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
