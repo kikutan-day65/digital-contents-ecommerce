2c057be21898102576cc0ffd2d05c3c4465b729d
Haruto Mori <baystarsman@outlook.jp>
2023-05-05 21:30:50 +0900

    add str funciton into product model

--- products/models.py
+++ products/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from users.models import Profile
 
 # Create your models here.
 class Product(models.Model):
+    owner = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL)
     product_name = models.CharField(max_length=50, null=False, blank=False)
     product_format = models.CharField(max_length=50, null=False, blank=False)
     product_description = models.CharField(max_length=1000, null=True, blank=True)
+
+    def __str__(self):
+        return str(self.product_name)
