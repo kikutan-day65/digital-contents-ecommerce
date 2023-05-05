1f76ee4610d40c28f3b09a9fb9f00ffa2c44c5da
Haruto Mori <baystarsman@outlook.jp>
2023-05-05 15:57:55 +0900

    fix: change the database to sqliste3

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysql',
-        'USER': "root",
-        'PASSWORD': config('MySQL_PASSWORD'),
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
