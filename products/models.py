dfaae5fbd0c47fcafc9badd65d18240c02f1891c
Haruto Mori <baystarsman@outlook.jp>
2023-05-05 21:00:15 +0900

    create a product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class product(models.Model):
+    product_name = models.CharField(max_length=50, null=False, blank=False)
+    product_format = models.CharField(max_length=50, null=False, blank=False)
+    product_description = models.CharField(max_length=1000, null=True, blank=True)
