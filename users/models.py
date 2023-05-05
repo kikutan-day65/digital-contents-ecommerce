b4aa362c1702ec4124477b514fcab131a3b1942a
Haruto Mori <baystarsman@outlook.jp>
2023-05-05 15:28:47 +0900

    feat: create a user profile model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    first_name = models.CharField(max_length=30, null=False, blank=False)
+    last_name = models.CharField(max_length=30, null=False, blank=False)
+    email = models.EmailField(max_length=50, null=False, blank=False, unique=True)
+    address = models.CharField(max_length=50, null=True, blank=True)
+    zip = models.IntegerField(null=True, blank=True)
+    phone = models.IntegerField(null=True, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
