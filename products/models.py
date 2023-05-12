f12e328c8c3b7a42b558dc49618427d038365f6a
Haruto Mori <baystarsman@outlook.jp>
2023-05-12 22:24:13 +0900

    fix: change the model attributes

--- products/models.py
+++ products/models.py
@@ -7,10 +7,11 @@ class Product(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     format = models.CharField(max_length=50, null=False, blank=False)
     description = models.TextField(max_length=1000, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True, default="default.jpg")
     tags = models.ManyToManyField('Tag', blank=True)
 
     def __str__(self):
-        return str(self.product_name)
+        return str(self.name)
     
 class Tag(models.Model):
     tag_name = models.CharField(max_length=30)
