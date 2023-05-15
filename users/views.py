4f0b5a259d67040917172d67d610b26e3a05b8db
Haruto Mori <baystarsman@outlook.jp>
2023-05-15 16:56:44 +0900

    feat: change the name of functionin the view

--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 from products.models import Product
 
 # Create your views here.
-def index(request):
+def profile(request):
     products = Product.objects.all()
     context = {'products': products}
     return render(request, 'users/profile.html', context)
