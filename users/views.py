4282cd7e4bb65b1a91e3b36ab5e92100485b1e7d
Haruto Mori <baystarsman@outlook.jp>
2023-05-15 20:22:40 +0900

    feat: login authenticate

--- users/views.py
+++ users/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.models import User
 
+from .models import Profile
 from products.models import Product
 
 def user_login(request):
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = User.objects.get(username=username)
+        except:
+            print('Username does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('profile')
+        else:
+            print('Username or passwprd is incorrect')
+
     context = {}
     return render(request, 'users/login_register.html', context)
 
