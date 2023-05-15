12e77122597eda8f8fac016a65cb5bac0bb1c52d
Haruto Mori <baystarsman@outlook.jp>
2023-05-15 20:06:47 +0900

    feat: buid a login function and its url

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.user_login, name='user-login'),
     path('', views.profile, name='profile'),
 ]
--- users/views.py
+++ users/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 
 from products.models import Product
 
-# Create your views here.
+def user_login(request):
+    context = {}
+    return render(request, 'users/login_register.html', context)
+
+
+
+# user profile page
 def profile(request):
     products = Product.objects.all()
     context = {'products': products}
